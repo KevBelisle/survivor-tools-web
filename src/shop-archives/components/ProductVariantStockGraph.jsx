7946f42d649515ac47c1e68620cd6dd92c290777
KevBelisle <kev.belisle@gmail.com>
2025-11-14 16:31:48 -0500

    Remove charts

--- src/shop-archives/components/ProductVariantStockGraph.jsx
+++ src/shop-archives/components/ProductVariantStockGraph.jsx
@@ -1,71 +1,19 @@
-import { useColorModeValue } from '@chakra-ui/react'
-import React, { useMemo } from 'react'
-import { Line } from 'react-chartjs-2'
-import 'chartjs-adapter-date-fns'
+import { useColorModeValue, Text, Box } from '@chakra-ui/react'
+import React from 'react'
 
 const ProductVariantStockGraph = ({ stockHistory }) => {
-  const options = {
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          stepSize: 5,
-        },
-        grid: {
-          borderColor: useColorModeValue('#CBD5E0', '#2D3748'),
-          color: useColorModeValue('#CBD5E0', '#2D3748'),
-          tickColor: useColorModeValue('#CBD5E0', '#2D3748'),
-        },
-        ticks: {
-          color: useColorModeValue('#A0AEC0', '#4A5568'),
-        },
-      },
-      y: {
-        beginAtZero: true,
-        grid: {
-          borderColor: useColorModeValue('#CBD5E0', '#2D3748'),
-          color: useColorModeValue('#CBD5E0', '#2D3748'),
-          tickColor: useColorModeValue('#CBD5E0', '#2D3748'),
-        },
-        ticks: {
-          color: useColorModeValue('#A0AEC0', '#4A5568'),
-        },
-      },
-    },
-    plugins: {
-      title: {
-        display: false,
-      },
-      legend: {
-        display: false,
-      },
-    },
-    animation: {
-      duration: 0,
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-  }
-  const data = {
-    datasets: [
-      {
-        data: useMemo(
-          () =>
-            stockHistory.map((data) => ({
-              x: data.timestamp,
-              y: data.stock,
-            })),
-          [stockHistory]
-        ),
-        fill: false,
-        stepped: 'after',
-        borderColor: '#805AD5',
-        backgroundColor: '#B794F4',
-      },
-    ],
-  }
-
-  return <Line data={data} options={options}></Line>
+  return (
+    <Box
+      p={4}
+      textAlign="center"
+      bg={useColorModeValue('gray.50', 'gray.700')}
+      rounded="md"
+    >
+      <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.400')}>
+        Stock history chart temporarily disabled
+      </Text>
+    </Box>
+  )
 }
 
 export default ProductVariantStockGraph
